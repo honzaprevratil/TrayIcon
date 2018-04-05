8a6b7d05125a6ddb3ad86b35e1a5e43a18351315
honzikprevratil <honzikprevratil@gmail.com>
2018-04-05 20:01:13 +0200

    init

--- TrayIcon/App.xaml.cs
+++ TrayIcon/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
 
 namespace TrayIcon
@@ -13,5 +14,61 @@ namespace TrayIcon
     /// </summary>
     public partial class App : Application
     {
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private bool isExit;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+            CreateNotifyIcon();
+        }
+
+        private void CreateNotifyIcon()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+            notifyIcon.Icon = TrayIcon.Properties.Resources.MyIcon;
+            notifyIcon.Visible = true;
+
+            notifyIcon.ContextMenuStrip =
+              new System.Windows.Forms.ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Add("Start App").Click += (s, e) => ShowMainWindow();
+            notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide();
+            }
+        }
+
+        private void ExitApplication()
+        {
+            isExit = true;
+            MainWindow.Close();
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
     }
 }
